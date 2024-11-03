ec8c74273e3172d65455b770873e2922198c4a6b
48534 <48534@DESKTOP-SJ3QDS3>
2024-11-03 22:24:10 +0100

    Zrobiłem zadanie

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,31 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    std::unique_ptr< Resource > resourcePtr;
+
+public:
+    ResourceManager() : resourcePtr(std::make_unique< Resource >()) {}
+
+    double get() const
+    {
+        return resourcePtr->get(); // Wywo³anie metody get() z obiektu Resource
+    }
+
+    // Konstruktor kopiuj¹cy
+    ResourceManager(const ResourceManager& other)
+        : resourcePtr(std::make_unique< Resource >(*other.resourcePtr))
+    {}
+
+    // Operator przypisania kopiuj¹cego
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            resourcePtr = std::make_unique< Resource >(*other.resourcePtr);
+        }
+        return *this;
+    }
+
+    // Domyœlny destruktor
+    ~ResourceManager() = default;
 };
