b4872b8bf509d882cf3ee35f60719bad52a73eaf
48534 <48534@DESKTOP-SJ3QDS3>
2024-12-15 21:25:11 +0100

    Zrobiłem zadanie

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,34 +1,40 @@
 #pragma once
 
 #include "Resource.hpp"
+#include <memory> // std::unique_ptr
 
 class ResourceManager
 {
 private:
-    std::unique_ptr< Resource > resourcePtr;
+    std::unique_ptr< Resource > resource; // Zarz¹dzany zasób (wskaŸnik unikalny)
 
 public:
-    ResourceManager() : resourcePtr(std::make_unique< Resource >()) {}
+    // Konstruktor domyœlny: inicjalizuje nowy zasób
+    ResourceManager() : resource(std::make_unique< Resource >()) {}
 
-    double get() const
-    {
-        return resourcePtr->get(); // Wywo³anie metody get() z obiektu Resource
-    }
-
-    // Konstruktor kopiuj¹cy
+    // Konstruktor kopiuj¹cy: kopiuje zasób
     ResourceManager(const ResourceManager& other)
-        : resourcePtr(std::make_unique< Resource >(*other.resourcePtr))
+        : resource(std::make_unique< Resource >(*other.resource))
     {}
 
-    // Operator przypisania kopiuj¹cego
+    // Operator przypisania kopiuj¹cy
     ResourceManager& operator=(const ResourceManager& other)
     {
         if (this != &other) {
-            resourcePtr = std::make_unique< Resource >(*other.resourcePtr);
+            resource = std::make_unique< Resource >(*other.resource);
         }
         return *this;
     }
 
-    // Domyœlny destruktor
+    // Konstruktor przenosz¹cy: przejmuje zasób
+    ResourceManager(ResourceManager&& other) noexcept = default;
+
+    // Operator przypisania przenosz¹cy: przejmuje zasób
+    ResourceManager& operator=(ResourceManager&& other) noexcept = default;
+
+    // Destruktor: automatycznie zwalnia zasób
     ~ResourceManager() = default;
+
+    // Metoda get() zwracaj¹ca wynik wywo³ania get() zarz¹dzanego zasobu
+    double get() const { return resource->get(); }
 };
